0007240a56213dc442f062362075026fedb8128e
ahmedtechm <ahmedtechm2050@gmail.com>
2023-05-11 12:08:37 +0400

    first commit

--- Salary-System/src/main/java/com/codeline/Salary/System/Models/Account.java
+++ Salary-System/src/main/java/com/codeline/Salary/System/Models/Account.java
@@ -1,4 +1,18 @@
 package com.codeline.Salary.System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Data
+@Entity
+
 public class Account {
+
+
+
 }
--- Salary-System/src/main/java/com/codeline/Salary/System/Models/BaseEntity.java
+++ Salary-System/src/main/java/com/codeline/Salary/System/Models/BaseEntity.java
@@ -1,4 +1,21 @@
 package com.codeline.Salary.System.Models;
 
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.MappedSuperclass;
+import java.util.Date;
+
+@MappedSuperclass
+@Getter
+@Setter
+@Data
+
 public class BaseEntity {
+
+    Date createdDate;
+    Date updatedDate;
+    Boolean isActive;
 }
